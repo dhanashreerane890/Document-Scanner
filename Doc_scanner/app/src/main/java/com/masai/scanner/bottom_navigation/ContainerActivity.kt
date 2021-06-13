df064d55ec0fc1a95031c24663592cd902c1a1c1
Varun Wani <varunwani22@gmail.com>
2021-06-13 14:23:08 +0530

    Complete navigation layout

--- Doc_scanner/app/src/main/java/com/masai/scanner/bottom_navigation/ContainerActivity.kt
+++ Doc_scanner/app/src/main/java/com/masai/scanner/bottom_navigation/ContainerActivity.kt
@@ -1,14 +1,16 @@
 package com.masai.scanner.bottom_navigation
 
 import android.content.res.Configuration
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.ImageButton
+import android.widget.SearchView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
@@ -16,12 +18,19 @@ import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import com.masai.scanner.R
+import com.masai.scanner.side_drawer.SettingFragment
 import kotlinx.android.synthetic.main.navigation_header.*
+
 
 class ContainerActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var fragmentManager: FragmentManager
+    private lateinit var nav: NavigationView
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
+    private lateinit var headerView: View
+    var navigationFragment: Fragment? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,29 +39,14 @@ class ContainerActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
         loadFragment(Fragment())
         fragmentManager = supportFragmentManager
 
-        val bottomNavigationView = findViewById<View>(R.id.bottomNavigationView) as BottomNavigationView
-        bottomNavigationView.setOnNavigationItemSelectedListener {
-            var navigationFragment: Fragment? = null
-            when(it.itemId){
-                R.id.home_navigation->{
-                    navigationFragment = HomeFragment()
-                }
-                R.id.pdfTools_navigation->{
-                    navigationFragment = PdfToolsFragment()
-                }
-            }
-            if (navigationFragment!=null){
-                loadFragment(navigationFragment)
-            }
-            return@setOnNavigationItemSelectedListener true
-        }
+        bottomNavigationWorking()
 
-
-        val toolbar: Toolbar = findViewById(R.id.toolbar0)
+        toolbar = findViewById(R.id.toolbar0)
         setSupportActionBar(toolbar)
 
-        val nav: NavigationView = findViewById(R.id.navMenu)
+        nav = findViewById(R.id.navMenu)
         nav.setNavigationItemSelectedListener(this)
+
 
         drawerLayout = findViewById(R.id.drawer)
 
@@ -60,7 +54,30 @@ class ContainerActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
-        val text1 = nav.findViewById<TextView>(R.id.signIn)
+        headerView = nav.getHeaderView(0)
+
+        val signin = headerView.findViewById<TextView>(R.id.signIn)
+        //write code of firebase to set username to sign in user
+        signin.text = "ABCDEF"
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun bottomNavigationWorking() {
+        val bottomNavigationView =
+            findViewById<View>(R.id.bottomNavigationView) as BottomNavigationView
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home_navigation -> {
+                    navigationFragment = HomeFragment()
+                }
+                R.id.pdfTools_navigation -> {
+                    navigationFragment = PdfToolsFragment()
+                }
+            }
+            navigationFragment?.let { loadFragment(it) }
+            return@setOnNavigationItemSelectedListener true
+        }
     }
 
     private fun loadFragment(navigationFragment: Fragment) {
@@ -74,6 +91,7 @@ class ContainerActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
         super.onPostCreate(savedInstanceState)
         toggle.syncState()
     }
+
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
         toggle.onConfigurationChanged(newConfig)
@@ -85,6 +103,7 @@ class ContainerActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
         }
         return super.onOptionsItemSelected(item)
     }
+
     override fun onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START)
@@ -93,14 +112,11 @@ class ContainerActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
         }
     }
 
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.setting -> Toast.makeText(
-                this,
-                "setting frgment need to added",
-                Toast.LENGTH_SHORT
-            ).show()
-            R.id.shareApp -> Toast.makeText(
+            R.id.setting -> navigationFragment = SettingFragment()
+            R.id.shareThisApp -> Toast.makeText(
                 this,
                 "share app frgment need to added",
                 Toast.LENGTH_SHORT
@@ -108,6 +124,18 @@ class ContainerActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
             //all content should add here
         }
         drawerLayout.closeDrawer(GravityCompat.START)
+        navigationFragment?.let { loadFragment(it) }
         return true
     }
+
+    //need to set listener
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        val menuItem = menu?.findItem(R.id.searchBar)
+        val searchView = menuItem?.actionView as SearchView
+        searchView.queryHint
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
 }
